3eb3a7c292e2c1db6264c03d2bd8f4708ee04330
Sehyun_Kim <kimsparadise0202@gmail.com>
2026-02-05 14:55:34 +0900

    fix: add necessary methods and configurations for User, PointTransaction

---
+++ src/main/java/com/paymentteamproject/domain/pointTransactions/entity/PointTransaction.java
@@ -0,0 +1,61 @@
+package com.paymentteamproject.domain.pointTransactions.entity;
+
+import com.paymentteamproject.common.entity.BaseEntity;
+import com.paymentteamproject.domain.pointTransactions.enums.TransactionType;
+import com.paymentteamproject.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name="point_transactions")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PointTransaction extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(nullable = false)
+    private double points;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionType type; //enum 구현 전
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(nullable = false)
+    private boolean validity;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime deletedAt;
+
+    public PointTransaction(User user, Order order, double points, TransactionType type, LocalDateTime expiresAt){
+        this.user = user;
+        this.order = order;
+        this.points = points;
+        this.type = type;
+        this.expiresAt = expiresAt;
+        this.validity = true;
+    }
+
+    public void softDelete(){
+        this.deletedAt = LocalDateTime.now();
+    }
+}
--- src/main/java/com/paymentteamproject/domain/user/entity/User.java
+++ src/main/java/com/paymentteamproject/domain/user/entity/User.java
@@ -18,27 +18,41 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String username;
 
+    @Column(nullable = false)
     private String phone;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private double pointBalance;
 
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     private LocalDateTime deletedAt;
 
     public User(String username, String phone, String email, String password, double pointBalance){
+
         this.username = username;
+
         this.phone = phone;
+
         this.email = email;
+
         this.password = password;
+
         //가입 시 관리자 임의로 포인트 지급 가능(가입 이벤트 등)
         this.pointBalance = pointBalance;
+    }
+
+    public void softDelete(){
+        this.deletedAt = LocalDateTime.now();
     }
 }
