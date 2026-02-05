fc71d04b1faffa1e620091fde1646313364fd649
곽현민 <hunmin111@gmail.com>
2026-02-05 10:59:14 +0900

    init

---
+++ src/main/java/com/paymentteamproject/PaymentTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.paymentteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentTeamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
