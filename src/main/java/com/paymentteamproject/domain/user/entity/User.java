2b8dafaa100db495e1330bc4acaf213ee55d4be9
Sehyun_Kim <kimsparadise0202@gmail.com>
2026-02-05 14:14:50 +0900

    add(domain): add User and PointTransaction domains

---
+++ src/main/java/com/paymentteamproject/domain/user/entity/User.java
@@ -0,0 +1,44 @@
+package com.paymentteamproject.domain.user.entity;
+
+import com.paymentteamproject.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name="users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String phone;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private double pointBalance;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime deletedAt;
+
+    public User(String username, String phone, String email, String password, double pointBalance){
+        this.username = username;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        //가입 시 관리자 임의로 포인트 지급 가능(가입 이벤트 등)
+        this.pointBalance = pointBalance;
+    }
+}
