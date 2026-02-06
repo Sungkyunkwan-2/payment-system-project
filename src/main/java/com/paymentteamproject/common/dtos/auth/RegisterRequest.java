bd6c4fbb5baa079f094124b0eb4d3d079f547b63
Sehyun_Kim <kimsparadise0202@gmail.com>
2026-02-06 15:53:07 +0900

    refactor: moved Register dtos to dtos/auth directory

---
+++ src/main/java/com/paymentteamproject/common/dtos/auth/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.paymentteamproject.common.dtos.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class RegisterRequest {
+    @NotBlank(message = "사용자명은 필수 입력 항목입니다.")
+    @Size(min = 2, message = "사용자명은 2자 이상이어야 합니다.")
+    private String name;
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
+    private String password;
+    @NotBlank(message = "전화번호는 필수 입력 항목입니다.")
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호는 010-xxxx-xxxx 형식이어야 합니다.")
+    private String phone;
+}
