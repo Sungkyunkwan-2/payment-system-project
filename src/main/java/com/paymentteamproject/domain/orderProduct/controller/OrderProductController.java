e05c24758d332ac32c7e55b6dbc851b6dd153793
김대훈 <eogns136@gmail.com>
2026-02-06 15:52:59 +0900

    fix: get order api fix

--- src/main/java/com/paymentteamproject/domain/orderProduct/controller/OrderProductController.java
+++ src/main/java/com/paymentteamproject/domain/orderProduct/controller/OrderProductController.java
@@ -3,9 +3,11 @@ package com.paymentteamproject.domain.orderProduct.controller;
 
 import com.paymentteamproject.domain.orderProduct.dto.getAllOrderProductResponse;
 import com.paymentteamproject.domain.orderProduct.service.OrderProductService;
+import com.paymentteamproject.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,11 @@ import java.util.List;
 public class OrderProductController {
     private final OrderProductService orderProductService;
 
-    //주문 목록 조회
+    //내 주문 목록 조회
     @GetMapping("/orders")
-    public ResponseEntity<List<getAllOrderProductResponse>> getAllOrderProducts() {
-        return ResponseEntity.status(HttpStatus.OK).body(orderProductService.getAllOrderProducts());
+    public ResponseEntity<List<getAllOrderProductResponse>> getAllOrderProducts(
+            @AuthenticationPrincipal User user
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(orderProductService.getAllOrderProducts(user.getId()));
     }
 }
