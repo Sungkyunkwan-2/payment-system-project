895e3961061c0eabd182c3a1e1d7cce410739501
김대훈 <eogns136@gmail.com>
2026-02-10 12:42:51 +0900

    add: Implementing a membership initialization function for registered members upon signing up

--- src/main/java/com/paymentteamproject/domain/pointTransactions/entity/PointTransactionType.java
+++ src/main/java/com/paymentteamproject/domain/pointTransactions/entity/PointTransactionType.java
@@ -1,6 +1,6 @@
 package com.paymentteamproject.domain.pointTransactions.entity;
 
-public enum TransactionType {
+public enum PointTransactionType {
     USED, //구매 시 포인트 사용
     RECOVERED, //구매 취소로 인한 포인트 복구
     ADDED, //포인트 적립
