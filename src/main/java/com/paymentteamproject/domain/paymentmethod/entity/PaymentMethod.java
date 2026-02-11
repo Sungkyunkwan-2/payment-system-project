1adb1c69d54594e2ee53f7d00ccd1915e5ac4e37
jihyeon <yio1346@gmail.com>
2026-02-11 12:56:35 +0900

    add: ADD subscription, Billing, PaymentMethod entity

---
+++ src/main/java/com/paymentteamproject/domain/paymentmethod/entity/PaymentMethod.java
@@ -0,0 +1,56 @@
+package com.paymentteamproject.domain.paymentmethod.entity;
+
+import com.paymentteamproject.common.entity.BaseEntity;
+import com.paymentteamproject.domain.paymentmethod.consts.PaymentMethodStatus;
+import com.paymentteamproject.domain.paymentmethod.consts.PgProvider;
+import com.paymentteamproject.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "payment_methods")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PaymentMethod extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String billingKey;
+
+    @Column(nullable = false)
+    private String customerUid;
+
+    @Column(nullable = false)
+    private PgProvider pgProvider;
+
+    @Column(nullable = false)
+    private boolean isDefault;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentMethodStatus status;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    public PaymentMethod(User user, String billingKey, String customerUid, PgProvider pgProvider, PaymentMethodStatus status) {
+        this.user = user;
+        this.billingKey = billingKey;
+        this.customerUid = customerUid;
+        this.pgProvider = pgProvider;
+        this.isDefault = false;
+        this.status = status;
+    }
+
+}
