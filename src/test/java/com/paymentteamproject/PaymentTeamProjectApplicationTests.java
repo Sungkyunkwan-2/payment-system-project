81b64f415e3253b9f7f6f2be8c2e6dc29d4c31ce
곽현민 <hunmin111@gmail.com>
2026-02-13 15:56:14 +0900

    fix: test application profile

--- src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
+++ src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
@@ -2,8 +2,10 @@ package com.paymentteamproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = "spring.cloud.aws.region.static=ap-northeast-2")
+@ActiveProfiles("test")
 class PaymentTeamProjectApplicationTests {
 
     @Test
