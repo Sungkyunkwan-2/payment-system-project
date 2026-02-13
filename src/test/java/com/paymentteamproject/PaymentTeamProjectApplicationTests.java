83043bd9aaa2ddb0e58e11d41b48ba5c9cae7305
곽현민 <hunmin111@gmail.com>
2026-02-13 15:13:05 +0900

    fix: test db ignore

--- src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
+++ src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
@@ -1,9 +1,11 @@
 package com.paymentteamproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = "spring.cloud.aws.region.static=ap-northeast-2")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PaymentTeamProjectApplicationTests {
 
     @Test
