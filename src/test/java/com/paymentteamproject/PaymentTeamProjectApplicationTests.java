a7bcf2ff1684e2436d08b91894066102904198e9
곽현민 <hunmin111@gmail.com>
2026-02-13 15:50:02 +0900

    fix: add application test yml

--- src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
+++ src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
@@ -1,7 +1,6 @@
 package com.paymentteamproject;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = "spring.cloud.aws.region.static=ap-northeast-2")
