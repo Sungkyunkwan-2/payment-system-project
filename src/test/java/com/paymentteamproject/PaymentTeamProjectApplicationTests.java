e79faba78f93a7190112a2bb20b8fbf7d7badb55
곽현민 <hunmin111@gmail.com>
2026-02-13 15:06:03 +0900

    fix: add aws region

--- src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
+++ src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.paymentteamproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.cloud.aws.region.static=ap-northeast-2")
 class PaymentTeamProjectApplicationTests {
 
     @Test
