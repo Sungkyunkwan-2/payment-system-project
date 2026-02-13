e83c403b66473871adce7f6e762ab50fb1648925
곽현민 <hunmin111@gmail.com>
2026-02-13 16:28:34 +0900

    fix: application test base64 decoding

--- src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
+++ src/test/java/com/paymentteamproject/PaymentTeamProjectApplicationTests.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = "spring.cloud.aws.region.static=ap-northeast-2")
+@SpringBootTest(
+        properties = {
+                "spring.cloud.aws.region.static=ap-northeast-2",
+                "spring.config.import="  // 빈 문자열로 완전히 무효화
+        }
+)
 @ActiveProfiles("test")
 class PaymentTeamProjectApplicationTests {
 
